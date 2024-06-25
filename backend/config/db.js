9dd32ddba3f15cb85646268c5d8b6d733fc1a2b2
avid <avdheshojha7@gmail.com>
2024-06-25 15:47:54 +0530

    mongo db.js secure

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,7 @@
 import mongoose, { mongo } from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://foodel:okFUsoEKIxXsUlsz@cluster0.rdespwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log('DB Connection established'));
+    await mongoose.connect('mongodb+srv://foodel:<password>@cluster0.rdespwv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>console.log('DB Connection established'));
 }
 
 
-// okFUsoEKIxXsUlsz
-// foodel - username
